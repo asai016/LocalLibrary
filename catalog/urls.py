2e9c07bdf9569effb8fd94e676cbcb8cc9ecc086
ansss <ansss060@gmail.com>
2025-11-08 10:16:03 +0700

    Ready for Heroku deployment

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,20 +1,27 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+
+    # CRUD для книг
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
+
+    # CRUD для авторов
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+
+    # Заимствованные книги
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('all-borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # ← ИСПРАВЛЕНО!
+
+    # Обновление книг
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
-    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
-    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
-    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
-    path('book/create/', views.BookCreate.as_view(), name='book_create'),
-    # ДОБАВЬТЕ ЭТИ ДВЕ СТРОКИ
-    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
-    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
 ]
